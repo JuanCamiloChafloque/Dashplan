0b1be6fdeb275e5310d18bbdaa90054e9874301f
Camilo <camilochafloque@gmail.com>
2022-12-28 19:16:39 -0600

    Spring backend initialization

---
+++ backend/src/main/java/com/example/dashplan/DashplanApplication.java
@@ -0,0 +1,13 @@
+package com.example.dashplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashplanApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/dashplan/DashplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dashplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
