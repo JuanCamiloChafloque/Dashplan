e0cd65f6b7f3358fd7d77d96c644fc9282c5c510
Camilo <camilochafloque@gmail.com>
2022-12-29 19:34:04 -0600

    Added bootstrap to react project

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,7 +3,7 @@ import React from "react";
 const Dashboard = () => {
   return (
     <div>
-      <h1>Welcome to the dashboard</h1>
+      <h1 className="alert alert-warning">Welcome to the dashboard</h1>
     </div>
   );
 };
