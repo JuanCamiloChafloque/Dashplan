903dd4f2e9fa3b8a365dc13fb7ea2e8b1f393c73
Camilo <camilochafloque@gmail.com>
2022-12-31 15:31:03 -0600

    Created service and controller for adding a PT to a backlog

---
+++ backend/src/main/java/com/example/dashplan/services/ProjectTaskService.java
@@ -0,0 +1,41 @@
+package com.example.dashplan.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.dashplan.domain.Backlog;
+import com.example.dashplan.domain.ProjectTask;
+import com.example.dashplan.repositories.BacklogRepository;
+import com.example.dashplan.repositories.ProjectTaskRepository;
+
+@Service
+public class ProjectTaskService {
+    
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    @Autowired 
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        projectTask.setBacklog(backlog);
+        Integer backlogSeq = backlog.getPTSequence();
+        backlogSeq++;
+        backlog.setPTSequence(backlogSeq);
+        projectTask.setProjectSequence(projectIdentifier + "-" + backlogSeq);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+        //Set Priority
+        if(projectTask.getPriority() == null) {
+            projectTask.setPriority(3);
+        }
+
+        //Set Status
+        if(projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+            projectTask.setStatus("TO_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
+    }
+}
---
+++ backend/src/main/java/com/example/dashplan/web/BacklogController.java
@@ -0,0 +1,41 @@
+package com.example.dashplan.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dashplan.domain.ProjectTask;
+import com.example.dashplan.services.ProjectTaskService;
+import com.example.dashplan.services.ValidationErrorService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/backlog")
+@CrossOrigin
+public class BacklogController {
+    
+    @Autowired
+    private ProjectTaskService projectTaskService;
+
+    @Autowired
+    private ValidationErrorService errorService;
+
+    @PostMapping("/{backlog_id}")
+    public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask, @PathVariable String backlog_id, BindingResult result) {
+        ResponseEntity<?> errorMap = errorService.mapValidationService(result);
+        if(errorMap != null) {
+            return errorMap;
+        }
+
+        ProjectTask newProjectTask = projectTaskService.addProjectTask(backlog_id, projectTask);
+        return new ResponseEntity<ProjectTask>(newProjectTask, HttpStatus.CREATED);
+    }
+}
