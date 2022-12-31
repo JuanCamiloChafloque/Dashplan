446c6a9a464358d43f46d9acad72e022b9b08ecc
Camilo <camilochafloque@gmail.com>
2022-12-30 23:37:22 -0600

    Added validation and error styling to project creation form

--- frontend/src/components/project/AddProject.js
+++ frontend/src/components/project/AddProject.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import classnames from "classnames";
 import { createProject } from "../../actions/projectsActions";
 
 const AddProject = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const error = useSelector((state) => state.errors);
 
   const [projectName, setProjectName] = useState("");
   const [projectIdentifier, setProjectIdentifier] = useState("");
@@ -23,7 +26,9 @@ const AddProject = () => {
       end_date,
     };
     dispatch(createProject(newProject));
-    navigate("/");
+    if (!error) {
+      navigate("/");
+    }
   };
 
   return (
@@ -38,31 +43,48 @@ const AddProject = () => {
                 <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg "
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": error.projectName,
+                    })}
                     placeholder="Project Name"
                     name="projectName"
                     value={projectName}
                     onChange={(e) => setProjectName(e.target.value)}
                   />
+                  {error.projectName && (
+                    <p className="invalid-feedback">{error.projectName}</p>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": error.projectName,
+                    })}
                     placeholder="Unique Project ID"
                     name="projectIdentifier"
                     value={projectIdentifier}
                     onChange={(e) => setProjectIdentifier(e.target.value)}
                   />
+                  {error.projectIdentifier && (
+                    <p className="invalid-feedback">
+                      {error.projectIdentifier}
+                    </p>
+                  )}
                 </div>
                 <div className="form-group">
                   <textarea
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": error.projectName,
+                    })}
                     placeholder="Project Description"
                     name="description"
                     value={description}
                     onChange={(e) => setDescription(e.target.value)}
                   ></textarea>
+                  {error.description && (
+                    <p className="invalid-feedback">{error.description}</p>
+                  )}
                 </div>
                 <h6>Start Date</h6>
                 <div className="form-group">
