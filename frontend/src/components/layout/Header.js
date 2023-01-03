d971efef9ea538faeaae7a0d51a11e77972f233b
Camilo <camilochafloque@gmail.com>
2023-01-02 23:36:13 -0600

    Dynamic header for authenticated and guest users

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,7 +1,64 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../actions/authActions";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const { valid, user } = useSelector((state) => state.auth);
+
+  const onLogoutHandler = () => {
+    dispatch(logout());
+    window.location.href = "/";
+  };
+
+  const Authenticated = () => {
+    return (
+      <div className="collapse navbar-collapse" id="mobile-nav">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/dashboard">
+              Dashboard
+            </Link>
+          </li>
+        </ul>
+
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/dashboard">
+              <i className="fas fa-user-circle mr-1" />
+              {user.fullName}
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/logout" onClick={onLogoutHandler}>
+              Logout
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  const Guest = () => {
+    return (
+      <div className="collapse navbar-collapse" id="mobile-nav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/register">
+              Sign Up
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
   return (
     <>
       <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
@@ -17,29 +74,7 @@ const Header = () => {
           >
             <span className="navbar-toggler-icon" />
           </button>
-
-          <div className="collapse navbar-collapse" id="mobile-nav">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/dashboard">
-                  Dashboard
-                </Link>
-              </li>
-            </ul>
-
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/register">
-                  Sign Up
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">
-                  Login
-                </Link>
-              </li>
-            </ul>
-          </div>
+          {valid && user ? <Authenticated /> : <Guest />}
         </div>
       </nav>
     </>
